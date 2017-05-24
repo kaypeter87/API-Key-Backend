4d4ae4c341bd79f96b3524e422b258be94dacb8b
deuscode <deuscode87@gmail.com>
2017-05-24 00:40:04 -0400

    fix .env config path

--- apikey.js
+++ apikey.js
@@ -1,16 +1,17 @@
-var express = require('express');
-var app = express();
+// specify env file path
+require('dotenv').config({path: 'keys.env'});
+var http = require('http');
 
-// set port
-var port = process.env.PORT || 8080
+// console log API Key test
+function onRequest(request, response) {
+    var key = process.env.WEATHER_KEY;
+    console.log("You asked for pie!" + request.url);
+    console.log(key);
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Now here's your pie!");
+    response.end();
+}
 
-app.use(express.static(__dirname));
-
-// routes
-app.get("/", function(req, res) {
-    res.render("index");
-})
-
-app.listen(port, function() {
-    console.log("app, running");
-})
+// verify server is running
+http.createServer(onRequest).listen(8888);
+console.log("I'm alive!");
