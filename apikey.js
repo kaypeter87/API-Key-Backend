39f2b0f5dd8aa411064253a979a75df6495d2093
deuscode <deuscode87@gmail.com>
2017-06-01 21:17:46 -0400

    fix lat lon parameters

--- apikey.js
+++ apikey.js
@@ -4,18 +4,17 @@ var fetch = require('isomorphic-fetch');
 var express = require('express');
 var app = express();
 
-var weatherKey = process.env.WEATHER_KEY;
-var weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?units=imperial?APPID=' + weatherKey;
+var weatherURL = `http://api.openweathermap.org/data/2.5/weather?units=imperial&APPID=${process.env.WEATHER_KEY}`
 
-// console log API Key test
-app.get('/weather/:lat:lon', function (req, res) {
-    fetch(`${weatherURL}?lat=${req.params.lat}, &lon=${req.params.lon}`)
+// express callback function for JSON fetch
+app.get('/weather/:lat/:lon', function (req, res) {
+    fetch(`${weatherURL}&lat=${req.params.lat}&lon=${req.params.lon}`)
         .then(response => response.json())
         .then(weather => res.send(weather))
         .catch(error => res.send(error))
 })
 
-// verify server is running
+// verify server is running and dotenv test
 app.listen(3030, function () {
     console.log("I'm alive!");
     console.log(weatherURL);
