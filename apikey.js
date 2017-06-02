8df5b599e752b72f78bf302a529127c0fc0e53a7
deuscode <deuscode87@gmail.com>
2017-06-01 23:22:53 -0400

    add forecast call

--- apikey.js
+++ apikey.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 
 var weatherURL = `http://api.openweathermap.org/data/2.5/weather?units=imperial&APPID=${process.env.WEATHER_KEY}`
+var forecastURL = `http://api.openweathermap.org/data/2.5/forecast?units=imperial&APPID=${process.env.WEATHER_KEY}`
 
 // enable CORS
 app.use(function(req, res, next) {
@@ -19,6 +20,13 @@ app.get('/weather/:lat/:lon', function (req, res) {
     fetch(`${weatherURL}&lat=${req.params.lat}&lon=${req.params.lon}`)
         .then(response => response.json())
         .then(weather => res.send(weather))
+        .catch(error => res.send(error))
+})
+
+app.get('/forecast/:lat/:lon', function (req, res) {
+    fetch(`${forecastURL}&lat=${req.params.lat}&lon=${req.params.lon}`)
+        .then(response => response.json())
+        .then(forecast => res.send(forecast))
         .catch(error => res.send(error))
 })
 
